525a8b5ab4f302d8fa4a5cac28c8ea5edae7d0e0
LeeJuneHyun <dlwnsgus777@naver.com>
2020-01-23 22:22:03 +0900

    2020-01-23
    자바 해쉬테이블 구현
    -미완성-

--- java_data_structure/05_HashTable/src/HashTable/hashTablePractice.java
+++ java_data_structure/05_HashTable/src/HashTable/hashTablePractice.java
@@ -1,10 +1,48 @@
 package HashTable;
 
+import java.security.MessageDigest;
+
 public class hashTablePractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("syso");
+		HashTable ht = new HashTable(12);
 	}
+}
 
+class HashTable {
+	private Object[] hash_table;
+	
+	public HashTable(int size) {
+		// TODO Auto-generated constructor stub
+		hash_table = new Object[size];
+		for (int i = 0; i < size; i++) {
+			hash_table[i] = 0;
+		}
+	}
+	
+	public int hash_function(String data) {
+		MessageDigest md;
+		int hashNumber = 0;
+		
+		try {
+			md = MessageDigest.getInstance("SHA-256");
+			md.update(data.getBytes());
+			
+			byte[] mdToString = md.digest();
+			
+			for (int i = 0; i < mdToString.length; i++) {
+				byte mdToByte = mdToString[i];
+				hashNumber += (mdToByte & 0xff);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return hashNumber; 
+	}
+	
+	public int getKey(int data) {
+		return data % 12;
+	}
 }
