33bd361548681ac38718fff62df5dbee7a08403a
LeeJuneHyun <dlwnsgus777@naver.com>
2020-01-24 21:03:53 +0900

    2020-01-24
    자바 해쉬테이블
    완성

--- java_data_structure/05_HashTable/src/HashTable/hashTablePractice.java
+++ java_data_structure/05_HashTable/src/HashTable/hashTablePractice.java
@@ -1,12 +1,22 @@
 package HashTable;
 
 import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class hashTablePractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		HashTable ht = new HashTable(12);
+		ht.insert_data("Lee", "ABCD");
+		ht.insert_data("Lee", "BCBC");
+		ht.insert_data("Lee", "dvdv");
+		ht.insert_data("Andy", "andy's data");
+		Object data = ht.get_data("Lee");
+		Object andyData = ht.get_data("Andy");
+		System.out.println("this data is " +  data);
+		System.out.println("this data is " +  andyData);
 	}
 }
 
@@ -18,6 +28,12 @@ class HashTable {
 		hash_table = new Object[size];
 		for (int i = 0; i < size; i++) {
 			hash_table[i] = 0;
+		}
+	}
+	
+	public void desc() {
+		for(Object value: this.hash_table) {
+			System.out.print(value.toString() + " ");
 		}
 	}
 	
@@ -44,5 +60,51 @@ class HashTable {
 	
 	public int getKey(int data) {
 		return data % 12;
+	}
+	
+	public void insert_data(String key, Object data) {
+		int hashData = this.hash_function(key);
+		int tableKey = this.getKey(hashData);
+		ArrayList<Object> hashArr = new ArrayList();
+		Object[] key_value = {hashData, data};
+		hashArr.add(key_value);
+		
+		if (this.hash_table[tableKey] == (Object)0) {
+			this.hash_table[tableKey] = hashArr;
+		} else {
+			ArrayList<Object> originData = (ArrayList<Object>) this.hash_table[tableKey];
+			for (int i = 0; i < originData.size(); i++) {
+				Object[] dataArray = (Object[]) originData.get(i);
+				if (dataArray[0] == key) {
+					dataArray[1] = data;
+				} else {
+					originData.add(key_value);
+					return;
+				}
+			}
+
+		}
+	}
+	
+	public Object get_data(String key) {
+		int hashData = this.hash_function(key);
+		int tableKey = this.getKey(hashData);
+		Object[] data = null;
+		
+		if (this.hash_table[tableKey] == (Object)0) {
+			System.out.println("None Data");
+			return null;
+		} else {
+			ArrayList<Object> originData = (ArrayList<Object>) this.hash_table[tableKey];
+			for (int i = 0; i < originData.size(); i++) {
+				data = (Object[]) originData.get(i);
+				System.out.println(data[1].toString());
+				if (data[0] == key) {
+					System.out.println(data[1]);
+				}
+			}
+		}
+		return data[1];
+		
 	}
 }
