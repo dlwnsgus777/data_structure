082150e7c8e986a4a7383d776cb303083bb7465a
Lee, JunHyun <dlwnsgus777@naver.com>
2020-02-12 16:35:20 +0900

    queue 코드 수정

--- java_data_structure/02_Queue/src/QuerePractice.java
+++ java_data_structure/02_Queue/src/QuerePractice.java
@@ -3,18 +3,26 @@ import java.util.ArrayList;
 public class QuerePractice {
 
 	public static void main(String[] args) {
-		System.out.println("project Test");
 		Queue queue = new Queue();
 		
+		// 데이터 넣기
 		queue.enqueue(123);
-		queue.enqueue("asdasd");
-		queue.size();
-		queue.dequeue();
-		queue.dequeue();
-		queue.size();
+		queue.enqueue("queue");
+		
+		// 데이터 빼면서 삭제하기
+		queue.peek();	// 123
+		System.out.println(queue.dequeue());	// "123"
+		System.out.println(queue.dequeue());	// "queue"
+		
+		// 큐가 비어있는지 확인하기
+		System.out.println(queue.isEmpty());	// true -> 비어있다
+		
+		// 비어 있는 큐에서 데이터를 가져오려고 할때
+		System.out.println(queue.dequeue());
 	}
 }
 
+// 큐 구현
 class Queue {
 	private ArrayList<Object> queue = new ArrayList<>();
 
@@ -23,13 +31,23 @@ class Queue {
 	}
 	
 	public Object dequeue() {
+		if (this.queue.size() == 0) {
+			System.out.println("Queue가 비어있습니다. ");
+			return null;
+		}
 		Object data = this.queue.get(0);
 		this.queue.remove(0);
-		System.out.println(data);
 		return data;
 	}
 	
-	public void size() {
-		System.out.println(this.queue.size());
+	public void peek() {
+		System.out.println(this.queue.get(0));
+	}
+	
+	public Boolean isEmpty() {
+	    if (this.queue.size() == 0) {
+	      return true;
+	    }
+	    return false;
 	}
 }
