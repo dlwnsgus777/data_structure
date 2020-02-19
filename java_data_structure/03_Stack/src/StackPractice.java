725e0f9123da17d97d0ac6fbf57d7be0042fd8fc
Lee, JunHyun <dlwnsgus777@naver.com>
2020-02-19 14:19:54 +0900

    스택 List사용 구현으로 변경

--- java_data_structure/03_Stack/src/StackPractice.java
+++ java_data_structure/03_Stack/src/StackPractice.java
@@ -1,42 +1,72 @@
+import java.util.ArrayList;
 
 public class StackPractice {
 
 	public static void main(String[] args) {
-		Stack stack = new Stack(10);
+		Stack stack = new Stack();
 		
 		stack.push(1);
 		stack.push(2);
 		stack.push(3);
 		
-		System.out.println(stack.peek());
+		stack.peek();
 		System.out.println(stack.pop());
-		System.out.println(stack.peek());
+		stack.peek();
 		System.out.println(stack.pop());
-		System.out.println(stack.peek());
+		stack.peek();
+		System.out.println(stack.pop());
 		System.out.println(stack.pop());
 	}
 
 }
 
 class Stack {
-	private int[] stackArray;
-	int lastIndex;
+	private ArrayList<Object> stackArray;
 	
-	public Stack(int length) {
-		this.stackArray = new int[length];
-		this.lastIndex = 0;
+	public Stack() {
+		this.stackArray = new ArrayList<Object>();
 	}
 	
 	public void push(int param) {
-		this.stackArray[lastIndex] = param;
-		this.lastIndex += 1;
+		this.stackArray.add(param);
 	}
 	
-	public int pop() {
-		return stackArray[--lastIndex];
+	public Object pop() {
+		if (stackArray.size() == 0) {
+			System.out.println("Stack is Empty");
+			return null;
+		} 
+		
+		Object data = this.stackArray.get(stackArray.size() - 1);
+		this.stackArray.remove(stackArray.size() - 1);
+		return data;
 	}
 	
-	public int peek() {
-		return stackArray[lastIndex - 1];
+	public void peek() {
+		System.out.println("Last Data : " + this.stackArray.get(stackArray.size() - 1));
 	}
 }
+
+
+//class Stack {
+//	private int[] stackArray;
+//	int lastIndex;
+//	
+//	public Stack(int length) {
+//		this.stackArray = new int[length];
+//		this.lastIndex = 0;
+//	}
+//	
+//	public void push(int param) {
+//		this.stackArray[lastIndex] = param;
+//		this.lastIndex += 1;
+//	}
+//	
+//	public int pop() {
+//		return stackArray[--lastIndex];
+//	}
+//	
+//	public int peek() {
+//		return stackArray[lastIndex - 1];
+//	}
+//}
